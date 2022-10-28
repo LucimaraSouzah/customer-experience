9795c6544f1ee6f83365170764783a00c9e20265
Lucimara <lucimarasouzah@gmail.com>
2022-10-28 12:16:34 -0300

    Desafio

--- customer-experience/src/register/register.service.ts
+++ customer-experience/src/register/register.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class RegisterService {
   async register(req): Promise<string> {
-    return `Cliente ${req.name} com sucesso!`;
+    return `Cliente ${req.name} criado com sucesso!`;
   }
 }
